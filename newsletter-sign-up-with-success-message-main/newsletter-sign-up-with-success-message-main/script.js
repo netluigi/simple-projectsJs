d0ab5a17ba9434f3aac7fecebb53d16754302381
netluigi <luiz.anfe@hotmail.com>
2023-06-08 09:59:44 +0100

    aplicacao de validacao de email finalizada

--- newsletter-sign-up-with-success-message-main/newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/newsletter-sign-up-with-success-message-main/script.js
@@ -1,35 +1,32 @@
 
 const confirmBtn=document.querySelector('.confirmBtn');
 const btnEmail= document.querySelector('#email');
-
-
 function emailValidation(event){
     const checkEmail= /\w+@\w+\.\w+/;
     const result = checkEmail.test(event)
     return result;
-
 }
-
-
 function confirmBtnDesk(event){
     event.preventDefault();
     let btnEmailValue= btnEmail.value;
     if(emailValidation(btnEmailValue)==true){
         const card=document.querySelector('.card');
         const emailConfirmation=document.querySelector('.emailConfirmation_styles');
-        emailConfirmation.style.display='block'
+        emailConfirmation.style.display='block';
         card.classList.add('card_submit');
         card.classList.toggle('card');
-    }else if (btnEmailValue.length < 1) {
-        alert('Erro: O campo de e-mail está vazio.');
-    }else if(emailValidation(btnEmailValue)==false){
-        console.log('cliquei')
-    }
-   
+    }else if (btnEmailValue.length < 1 || emailValidation(btnEmailValue)==false) {
+        const textErro=document.querySelector('.fomr_p_Error');
+        const styleSheet= new CSSStyleSheet();
+        btnEmail.style.background='rgb(244, 196, 196)';
+        btnEmail.style.border='1px solid rgb(206, 61, 42)';
+        styleSheet.insertRule('::placeholder{color:red;}');
+        document.adoptedStyleSheets=[styleSheet];   
+        textErro.style.display='block';
+        textErro.style.fontFamily='Roboto, sans-serif';
+        textErro.style.color='rgb(206, 61, 42)';
+        textErro.style.fontSize='10px';
+        textErro.style.fontWeight='700';
+    }  
 }
-
-function confirmBtnMobile(){
-
-}
-
 confirmBtn.addEventListener('click',confirmBtnDesk)
